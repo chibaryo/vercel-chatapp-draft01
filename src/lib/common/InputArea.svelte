86f4223b96130bf2dc53a42e1a0d5d3bcd2af4a1
Ryo <swintschiba@gmail.com>
2024-01-02 09:38:44 +0900

    1st

--- src/lib/common/InputArea.svelte
+++ src/lib/common/InputArea.svelte
@@ -2,17 +2,22 @@
 
 </script>
 
-<section class="h-6[vh] flex flex-row items-center mx-4">
-  <form class="bg-white mx-2 px-4 py-4 w-11/12">
-    <input type="text" placeholder="Type your message..." class="shadow rounded-full w-full text-lg py-3 px-4" />
+<section class="h-6[vh] flex flex-row items-center mx-2">
+  <form
+    method="POST"
+    action="?/sendmsg"
+    class="bg-white px-4 py-4 flex flex-row gap-4 w-full">
+    <div class="w-11/12">
+      <input type="text" placeholder="Type your message..." class="shadow rounded-full text-lg py-3 px-4 w-full" />
+    </div>
+    <div class="w-12 rounded-full bg-sky-400 p-3">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        class="stroke-white fill-white">
+        <title>send-outline</title>
+        <path d="M4 6.03L11.5 9.25L4 8.25L4 6.03M11.5 14.75L4 17.97V15.75L11.5 14.75M2 3L2 10L17 12L2 14L2 21L23 12L2 3Z" />
+      </svg>
+    </div>
   </form>
-  <div class="w-12 rounded-full bg-sky-400 p-3">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      class="stroke-white fill-white">
-      <title>send-outline</title>
-      <path d="M4 6.03L11.5 9.25L4 8.25L4 6.03M11.5 14.75L4 17.97V15.75L11.5 14.75M2 3L2 10L17 12L2 14L2 21L23 12L2 3Z" />
-    </svg>
-  </div>
 </section>
